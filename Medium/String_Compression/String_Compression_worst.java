eae481a272a2cfa1c5d35603c6f7164e7e43ece4
Atharva321 <44301928+Atharva321@users.noreply.github.com>
2021-10-10 14:26:06 +0530

    Create String_Compression_worst.java

---
+++ Medium/String_Compression/String_Compression_worst.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int compress(char[] chars) {
+        String s = chars[0]+"";
+        int count = 1;
+        for(int i=0; i<chars.length-1;i++){
+            if(chars[i]==chars[i+1]){
+                count++;
+            }
+            else {
+                if(count>1){
+                    s+= count;
+                    count = 1;
+                }
+                s+=chars[i+1];
+            }
+            
+        }
+
+        if(count >1){
+            s+=count;
+            count =1;
+        }
+        
+        for(int i =0; i<s.length();i++){
+            chars[i] = s.charAt(i);
+        }
+        //System.out.println(chars);
+        return s.length();
+    }
+}
