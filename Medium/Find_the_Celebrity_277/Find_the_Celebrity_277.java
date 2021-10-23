d9fefcc1a183bdd13cb9d0a4c3aa475247d82b5f
Atharva321 <44301928+Atharva321@users.noreply.github.com>
2021-10-23 10:30:02 +0530

    Update Find_the_Celebrity_277.java

--- Medium/Find_the_Celebrity_277/Find_the_Celebrity_277.java
+++ Medium/Find_the_Celebrity_277/Find_the_Celebrity_277.java
@@ -8,12 +8,45 @@ public class Solution extends Relation {
      */
     public int findCelebrity(int n) {
         // Write your code here
+        int start = 0;
+        int end = n-1;
+        while (start < end) {
+            if (knows(start, end)) {
+                start++;
+            } else {
+                end--;
+            }
+        }
+         
+        // Validating the celebrity
+        int celebrity = end;
+        for(int person = 0; person<total_persons; person++){
+            if(person==celebrity) continue;
+            else if(knows(celebrity, person) || !knows(person, celebrity)) return -1;
+        }
+        return celebrity;
+          
     }
 }
 
 /*
+======================================================================
+O(n2)
+O(n)
+
+======================================================================
+Stack 
+
+
+======================================================================
+Graphs not implemented yet
+
+Using Indegree outdegree concept.
+O(n2)
 =======================================================================
-public int findCelebrity(int total_persons) {
+      
+      
+      public int findCelebrity(int total_persons) {
         // Write your code here
         int celebrity = 0;
         for(int person =1; person< total_persons; person++){
@@ -22,11 +55,10 @@ public int findCelebrity(int total_persons) {
             }
         }
         for(int person = 0; person<total_persons; person++){
-            if((person!=celebrity && knows(celebrity, person)) || !knows(person, celebrity)){
-                return -1;
-            }
+            if(person==celebrity) continue;
+            else if(knows(celebrity, person) || !knows(person, celebrity)) return -1;
         }
         return celebrity;
-    }
+      }
 */
 
